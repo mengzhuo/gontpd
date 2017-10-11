a1045f5f18d9ef6640ab7482a5cc53a52f9510de
Meng Zhuo <mengzhuo1203@gmail.com>
2017-10-11 07:16:10 +0000

    add url

--- make.sh
+++ make.sh
@@ -7,4 +7,4 @@ mkdir -p .buildtmp/usr/bin
 
 go build -o .buildtmp/usr/bin/gontpd cmd/gontpd/main.go
 
-fpm -s dir -C '.buildtmp/' -t deb -n gontpd -v `git describe --tags` --verbose
+fpm -s dir -C '.buildtmp/' -t deb -n gontpd -v `git describe --tags` --verbose --url http://gontpd.org

fc6edae8f12fa1b55cf2cfcf4e978d20d4530155
Meng Zhuo <mengzhuo1203@gmail.com>
2018-07-10 09:48:47 +0800

    add go bin

--- make.sh
+++ make.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+set -ue
+
+MAKEGOBIN=${MAKEGOBIN:-`which go`}
+echo $MAKEGOBIN
+
 rm -rf *.deb
 rm -rf .buildtmp
 cp -r pkg .buildtmp
 mkdir -p .buildtmp/usr/bin
 
-go build -o .buildtmp/usr/bin/gontpd cmd/gontpd/main.go
+$MAKEGOBIN build -o .buildtmp/usr/bin/gontpd cmd/gontpd/main.go
 
-fpm -s dir -C '.buildtmp/' -t deb -n gontpd -v `git describe --tags` --verbose --url http://gontpd.org
+fpm -s dir -C '.buildtmp/' -t deb -n gontpd -v `git describe --tags` --verbose --url https://gontpd.org

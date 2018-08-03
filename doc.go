a08f4d3db0bc37df64102536d834b37972727712
Meng Zhuo <mengzhuo1203@gmail.com>
2018-08-03 03:04:21 -0400

    add doc

---
+++ doc.go
@@ -0,0 +1,10 @@
+/*
+	Package gontpd is a high performance NTP daemon written in Go.
+
+	NOTE: only support Linux for now.
+
+	You can install binary by
+
+		go get github.com/mengzhuo/gontpd/cmd/gontpd
+*/
+package gontpd
